499d2d50cb440923f28459850e7b737bc6d8eb74
Smith <Jack.Smith@landregistry.gov.uk>
2019-10-09 10:22:30 +0100

    Added Annotations sub project

--- spring-demo-annotations/src/com/luv2code/springdemo/HappyFortuneService.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/HappyFortuneService.java
@@ -1,11 +1,13 @@
 package com.luv2code.springdemo;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class HappyFortuneService implements FortuneService {
 
 	@Override
 	public String getFortune() {
-		// TODO Auto-generated method stub
-		return null;
+		return "Today is you lucky day";
 	}
 
 }
