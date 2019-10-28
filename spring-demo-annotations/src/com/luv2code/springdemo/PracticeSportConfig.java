23235a2ab7cdf95da386e2f2fd4d2da9363ce778
Smith <Jack.Smith@landregistry.gov.uk>
2019-10-28 14:10:57 +0000

    More Springboot completed

--- spring-demo-annotations/src/com/luv2code/springdemo/PracticeSportConfig.java
+++ spring-demo-annotations/src/com/luv2code/springdemo/PracticeSportConfig.java
@@ -6,13 +6,13 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class PracticeSportConfig {
 		
-	// define bean for our happy fortune service
+	// define bean for our good fortune service
 	@Bean
 	public FortuneService goodFortuneService() {
 		return new GoodFortuneService();
 	}
 	
-	// define bean for our swim coach AND inject dependency
+	// define bean for our OW coach AND inject dependency
 	@Bean
 	public Coach OWCoach() {
 		OWCoach myOWCoach = new OWCoach(goodFortuneService());
